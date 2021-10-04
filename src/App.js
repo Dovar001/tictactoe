690ed47a6d6ef37c5e2bb8ba876c4a2f81030fba
Dovar <dovarabdulhanonov@gmail.com>
2021-10-04 20:21:52 +0500

    ready to add a history

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React,{useState} from "react";
+import Board from "./components/Board";
+import { calculateWinner } from "./components/helpers";
 
 const App = () => {
+
+  const [history, setHistory] = useState([{board:Array(9).fill(null),isXNext:true}])
+
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+ 
+   const winner = calculateWinner(current.board);
+   
+   const message = winner ? `Winner is ${winner}`: `Next player is ${current.isXNext? 'X' : 'O'} `
+
+  const handleOnclick = (position) => {
+    if (current.board[position] || winner){
+          return;
+       }
+       setHistory(prev=>{
+
+        const last = prev[prev.length-1];
+        
+
+     const newBoard =  last.board.map((square,pos)=>{
+         if(pos===position){
+            return last.isXNext ? 'X':'O' 
+         }
+         return square;
+     })
+    return prev.concat({board:newBoard,isXNext:!last.isXNext});
+    
+  })
+  setCurrentMove(prev=>prev+1)
+ };
+
 return (
-   <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
+   <div className="app">
+     <h1>TIC TAC TOE</h1>
+     <h2>{message}</h2>
+    <Board  board={current.board} handleOnclick={handleOnclick} />
+  </div>
 );
 };
 
